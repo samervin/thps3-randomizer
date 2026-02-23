92fd475691741759c2322c6b78c970daf52b2d8f
Sam Ervin <samuelthomaservin@gmail.com>
2026-02-22 21:09:13 -0600

    reset line numbers on modified QBs for easier comparison

---
+++ thps3_randomizer.py
@@ -0,0 +1,22 @@
+def read_modified_qbs() -> tuple[str, ...]:
+    # airtricks used for trick type locking
+    # ajc used in trickspot trick randomization and impress pedestrian goals
+    # alf used in trickspot trick randomization and suburbia junk
+    # bdj used in trickspot trick randomization and impress pedestrian goals
+    # boardselect used to randomize decks
+    # cjr used in trickspot trick randomization and foundry gap randomization
+    # comp_scripts randomizes competition score goals and requires deck for medals
+    # cpf used in trickspot trick randomization
+    # gameflow used only to bypass save dialog after runs, not randomized
+    # game used to change level display requirements
+    # gamemode randomizes time limit for normal levels
+    # goal_scripts randomizes level unlock requirements and score goals,
+    #     as well as requiring the deck for the tape + randomizing secret unlocks
+    # judges used to require deck for medals
+    # levels used only to load files from the correct directory, not randomized
+    # mainmenu randomizes level unlock requirements
+    # protricks randomizes trick sets for all skaters
+    # sk3_pedscripts randomizes impress pedestrian goals
+    # skater_profile randomizes stats, trickstyles, specials,
+    #    and whether a skater starts locked, for all skaters
+    pass
